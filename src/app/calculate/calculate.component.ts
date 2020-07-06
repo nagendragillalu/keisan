6c44c2bf831ca116ef6bcfce200a078e8a9e8d21
Gillalu Nagendra <nagendra.gillalu@diva.co.jp>
2020-07-07 01:11:24 +0900

    UI Modification

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -11,44 +11,42 @@ export class CalculateComponent implements OnInit {
 
   FinalSum: any = 100000;
   BidAmount: any;
-  AnualROI: any ;
-  InHandAmmount: any;  
+  AnualROI: any;
+  InHandAmmount: any;
   RemainTenure: any = 12;
 
   ngOnInit(): void {
     this.BidAmount = this.FinalSum * 0.01;
     this.InHandAmmount = this.FinalSum - this.BidAmount;
-    this.AnualROI = this.BidAmount/(this.FinalSum * (this.RemainTenure/12))
+    this.AnualROI = this.BidAmount / (this.FinalSum * (this.RemainTenure / 12))
   }
 
   onKeyFinalAmt(event: any) {
-      this.FinalSum = event.target.value
-      this.BidAmount = this.FinalSum * 0.01;
-      this.InHandAmmount = this.FinalSum - this.BidAmount; 
-      this.AnualROI = this.BidAmount/(this.FinalSum * (this.RemainTenure/12))
+    this.FinalSum = event.target.value
+    this.BidAmount = this.FinalSum - this.InHandAmmount;
+    this.AnualROI = this.BidAmount / (this.FinalSum * (this.RemainTenure / 12));
   }
 
   onKeyTenure(event: any) {
     this.RemainTenure = event.target.value
-    this.AnualROI = this.BidAmount/(this.FinalSum * (this.RemainTenure/12))
-} 
+    this.AnualROI = this.BidAmount / (this.FinalSum * (this.RemainTenure / 12));
+  }
 
-onKeyBid(event: any) {
-  this.BidAmount = event.target.value;
-  this.InHandAmmount = this.FinalSum - this.BidAmount; 
-  this.AnualROI = this.BidAmount/(this.FinalSum * (this.RemainTenure/12));
-} 
+  onKeyBid(event: any) {
+    this.BidAmount = event.target.value;
+    this.InHandAmmount = this.FinalSum - this.BidAmount;
+    this.AnualROI = this.BidAmount / (this.FinalSum * (this.RemainTenure / 12));
+  }
 
-onKeyROI(event: any) {
-  this.AnualROI = event.target.value;
-  this.BidAmount = this.AnualROI * this.FinalSum* (this.RemainTenure/12)
-  this.InHandAmmount = this.FinalSum - this.BidAmount; 
-} 
+  onKeyROI(event: any) {
+    this.AnualROI = event.target.value;
+    this.BidAmount = this.AnualROI * this.FinalSum * (this.RemainTenure / 12)
+    this.InHandAmmount = this.FinalSum - this.BidAmount;
+  }
 
-onKeyInHand(event: any) {
-  this.InHandAmmount = event.target.value;
-  this.BidAmount = this.FinalSum - this.InHandAmmount;
-  this.AnualROI = this.BidAmount/(this.FinalSum * (this.RemainTenure/12))
-} 
-
+  onKeyInHand(event: any) {
+    this.InHandAmmount = event.target.value;
+    this.BidAmount = this.FinalSum - this.InHandAmmount;
+    this.AnualROI = this.BidAmount / (this.FinalSum * (this.RemainTenure / 12))
+  }
 }
