84f91de629f6ee5b055d39f6f2d9ac319878af27
nagendragillalu <nagendragillalu@gmail.com>
2023-05-25 23:05:48 +0900

    Refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'keisan-pwa'`, () => {
+  it(`should have as title 'keisan'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('keisan-pwa');
+    expect(app.title).toEqual('keisan');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('keisan-pwa app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('keisan app is running!');
   });
 });
